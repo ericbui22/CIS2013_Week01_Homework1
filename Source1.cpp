b5ef59dd0b66b2f206c97a02f8ae5ca30134467c
ericbui22 <35546841+ericbui22@users.noreply.github.com>
2018-01-24 16:09:41 -0600

    Add files via upload

--- Source1.cpp
+++ Source1.cpp
@@ -1,18 +1,19 @@
-#include (iostream)
+#include <iostream>
 using namespace std;
 
 int main()
-{
-	char something;
-	cout<< =============================================================================== >> \n;
-	cout<< Eric Bui	\n;
-	cout << Student, Oklahoma State University - OKC \n
-		\n
-		cout << 4609 Stag Horn Dr                     p : (714) - 837 - 3623 \n;
-	cout << Yukon, Oklahoma                           e : ericbui221@gmail.com \n;
-	cout<<                                            g:https://github.com/ericbui22 \n;
-	cout <<                                           t:@ericbui22 \n;
-	cout <<= ============================================================================= >> \n;
-	return 0;
+{	
+		cout << "===============================================================================    \n";
+		cout << "\n";
+		cout << "Eric Bui                                                                           \n";
+		cout << "Student, Oklahoma State University - OKC                                           \n";
+		cout << "4609 Stag Horn Dr                         p : (714)-837-3623                       \n";
+		cout << "Yukon, Oklahoma                           e : ericbui221@gmail.com                 \n";
+		cout << "                                          g : github.com/ericbui22                 \n";
+		cout << "                                          t : @ericbui22                           \n";
+		cout << "\n";
+		cout << "===============================================================================    \n";
+	
+			return 0;
 
 }
