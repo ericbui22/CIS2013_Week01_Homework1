808831a70e46fdcc7f88a387e1728ac32b0a102a
ericbui22 <ericbui221@gmail.com>
2018-01-24 15:10:32 -0600

    businesscard

--- Source1.cpp
+++ Source1.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main()
 {
 	char something;
-	cout<< ========================== >> \n;
+	cout<< =============================================================================== >> \n;
 	cout<< Eric Bui	\n;
-	cout<< Student, Oklahoma State University - OKC \n
-	\n
-	cout<< 4609 Stag Horn Dr                     p: (714)-837-3623 \n
-	cout<< Yukon, Oklahoma                       e: ericbui221@gmail.com \n
-	cout<<                                       g:https://github.com/ericbui22 \n
-	cout <<                                      t : @ericbui22 \n
-	cout <<= ======================== >> \n;
+	cout << Student, Oklahoma State University - OKC \n
+		\n
+		cout << 4609 Stag Horn Dr                     p : (714) - 837 - 3623 \n;
+	cout << Yukon, Oklahoma                           e : ericbui221@gmail.com \n;
+	cout<<                                            g:https://github.com/ericbui22 \n;
+	cout <<                                           t:@ericbui22 \n;
+	cout <<= ============================================================================= >> \n;
 	return 0;
 
 }
