f58f5f5e52a752b484e3eb32b5e06864d9916a9e
ericbui22 <ericbui221@gmail.com>
2018-01-24 15:07:39 -0600

    BusinessCard

---
+++ Source1.cpp
@@ -0,0 +1,18 @@
+#include (iostream)
+using namespace std;
+
+int main()
+{
+	char something;
+	cout<< ========================== >> \n;
+	cout<< Eric Bui	\n;
+	cout<< Student, Oklahoma State University - OKC \n
+	\n
+	cout<< 4609 Stag Horn Dr                     p: (714)-837-3623 \n
+	cout<< Yukon, Oklahoma                       e: ericbui221@gmail.com \n
+	cout<<                                       g:https://github.com/ericbui22 \n
+	cout <<                                      t : @ericbui22 \n
+	cout <<= ======================== >> \n;
+	return 0;
+
+}
